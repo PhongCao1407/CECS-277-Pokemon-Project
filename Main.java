b2ece2beb69fb6e890ecacb5a4e21bf4bd81f17f
Phong Cao <phongcao0714@gmail.com>
2021-10-13 16:06:38 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+    }
+
+    public static int mainMenu(){
+        //TODO
+        return 0;
+    }
+
+    public static Pokemon chooseRandomPokemon(){
+        //TODO
+    }
+
+    public static void trainerAttack(Trainer t, Pokemon wild){
+        //TODO
+        
+    }
+
+    public static void store(Trainer t){
+        //TODO
+    }
+}
