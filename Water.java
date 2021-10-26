e2c128c554eedd9fc29a481ba0bd6e82f302a1b4
j0rban <73670181+j0rban@users.noreply.github.com>
2021-10-25 17:49:14 -0700

    Update Water.java

--- Water.java
+++ Water.java
@@ -4,6 +4,8 @@ public interface Water {
     public String specialMenu = "1. Water Gun\n2. Bubble Beam\n3.Waterfall";
     public int numSpecialMenuItems = 3;
 
+    //Javadocs for these methods are in Squirtle and Staryu classes
+    
     public String waterGun(Pokemon p);
     public String bubbleBeam(Pokemon p);
     public String waterfall(Pokemon p);
