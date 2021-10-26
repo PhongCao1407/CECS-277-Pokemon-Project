95fec98bb7317bcff3b50838c48dfb0b6e14b4c1
Phong Cao <phongcao0714@gmail.com>
2021-10-25 17:33:43 -0700

    add documentation

--- Oddish.java
+++ Oddish.java
@@ -1,18 +1,35 @@
 public class Oddish extends Pokemon implements Grass {
     
+    /**
+     * Inherit from Pokemon superclass
+     * Declare pokemon type as Grass
+     */
     public Oddish(){
         super("Oddish");
         this.pokemonType = "Grass";
     }
 
+    /**
+     * @return the Special Menu for Grass type
+     */
     public String getSpecialMenu(){
         return Grass.specialMenu;
     }
 
+    /**
+     * @return the number of items in the Special Menu (3)
+     */
     public int getNumSpecialMenuItems(){
         return 3;
     }
 
+    /**
+     * Special Attack a Pokemon with a move given as an int 
+     * 1: vireWhip
+     * 2: razorLeaf
+     * 3: solarBeam
+     * @return the Special Attack message as a String
+     */
     public String specialAttack(Pokemon p, int move){
         switch (move){
             case 1:
@@ -30,6 +47,9 @@ public class Oddish extends Pokemon implements Grass {
         return p.toString();
     }
 
+    /**
+     * @return the attack damage message of Vine Whip
+     */
     public String vineWhip(Pokemon p){
         final int MIN_DAMAGE = 1;
         final int MAX_DAMAGE = 3;
@@ -40,6 +60,9 @@ public class Oddish extends Pokemon implements Grass {
         return p.getName() + " is WHIPPED with VINE and takes " +  damage + " damage";
     }
 
+    /**
+     * @return the attack damage message of Razor Leaf
+     */
     public String razorLeaf(Pokemon p){
         final int MIN_DAMAGE = 2;
         final int MAX_DAMAGE = 4;
@@ -50,6 +73,9 @@ public class Oddish extends Pokemon implements Grass {
         return p.getName() + "is SLASHED with RAZOR LEAF and takes " + damage + " damages";
     }
 
+    /**
+     * @return the attack damage message of Solar Beam
+     */
     public String solarBeam(Pokemon p){
         final int MIN_DAMAGE = 0;
         final int MAX_DAMAGE = 3;
