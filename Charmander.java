a194ea134b52087c5b001e545900c4823760c665
giovanna-ruiz <80977027+giovanna-ruiz@users.noreply.github.com>
2021-10-25 18:31:44 -0700

    Update Charmander.java

--- Charmander.java
+++ Charmander.java
@@ -6,15 +6,26 @@ public class Charmander extends Pokemon implements Fire {
 		super("Charmander");
 		this.pokemonType = "Fire";
 	}
-	
+   /**
+    * fire type special menu 
+    * @return Fire.specialMenu
+    */
     public String getSpecialMenu(){
         return Fire.specialMenu;
     }
-
+	
+   /**
+    * amount of options in special menu 
+    * @return 3
+    */
     public int getNumSpecialMenuItems(){
         return 3;
     }
-    
+	
+   /**
+    * a special attack move is selected by user
+    * @return pokemon info and attack message or invalid message
+    */    
     public String specialAttack(Pokemon p, int move){
         switch (move){
             case 1:
@@ -31,7 +42,11 @@ public class Charmander extends Pokemon implements Fire {
         }
         return p.toString();
     }
-
+	
+   	/**
+	 * attack type ember causes damage between the range 0-3
+	 * @return attack message
+	 */ 
 	@Override
 	public String ember(Pokemon p) {
 		int min = 0;
@@ -39,9 +54,13 @@ public class Charmander extends Pokemon implements Fire {
 		int damage = (int) ((Math.random() * (max - min)+1) + min);
         damage *= battleTable[0][p.getType()];
 		p.takeDamage(damage);
-		return "Charmander made an ember attack to " + p.getName() + " with damage " + damage;
+		return "Charmander made an EMBER ATTACK to " + p.getName() + " with damage " + damage;
 	}
 	
+   	/**
+	 * attack type fireBlast causes damage between the range 1-4
+	 * @return attack message
+	 */	
 	@Override
 	public String fireBlast(Pokemon p) {
 		int min = 1;
@@ -49,9 +68,13 @@ public class Charmander extends Pokemon implements Fire {
 		int damage = (int) ((Math.random() * (max - min)+1) + min);
         damage *= battleTable[0][p.getType()];
 		p.takeDamage(damage);
-		return "Charmander blasted fire to " + p.getName() + " with damage " + damage;
+		return "Charmander BLASTED FIRE to " + p.getName() + " with damage " + damage;
 	}
 	
+   	/**
+	 * attack type firePunch causes damage between the range 1-3
+	 * @return attack message
+	 */	
 	@Override
 	public String firePunch(Pokemon p) {
 		int min = 1;
@@ -59,6 +82,6 @@ public class Charmander extends Pokemon implements Fire {
 		int damage = (int) ((Math.random() * (max - min)+1) + min);
         damage *= battleTable[0][p.getType()];
 		p.takeDamage(damage);
-		return "With a flaming fist, Charmander punched " + p.getName() + " with damage " + damage;
+		return "With a FLAMING fist, Charmander PUNCHED " + p.getName() + " with damage " + damage;
 	}
 }
