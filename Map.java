ba3eb9bf8f9ac99ed9d476d9618d2123afa93bcd
j0rban <73670181+j0rban@users.noreply.github.com>
2021-10-25 17:45:54 -0700

    Update Map.java

--- Map.java
+++ Map.java
@@ -8,6 +8,10 @@ public class Map {
     private char [][] map = new char[5][5];
     private boolean [][] revealed = new boolean[5][5];
 
+    /**
+    * Map constructor
+    * initializes revealed array to false
+    */
     public Map(){
 
         for(int i = 0; i < 5; i++){
@@ -17,6 +21,10 @@ public class Map {
         }
     }
 
+    /**
+    * Loads in the chosen map from a text file to map array
+    * @param mapNum the number of the map to load
+    */
     public void loadMap(int mapNum){
 
         String fileName = "Area" + Integer.toString(mapNum) + ".txt";
@@ -43,10 +51,22 @@ public class Map {
         
     }
 
+    /**
+    * get character at a location on the map
+    * @param p Point coordinate to check
+    * @return character at the location
+    */
     public char getCharAtLoc(Point p){
         return map[p.y][p.x];
     }
 
+    /**
+    * Iterates through map and adds each char to a string
+    * if the location is not revealed, an 'x' is shown
+    * player location is shown with a '*'
+    * @param p Point location of player
+    * @return string with map
+    */
     public String mapToString(Point p){
         String mapString = "";
         for(int i = 0; i < 5; i++){
@@ -64,21 +84,25 @@ public class Map {
             }
             mapString += '\n';
         }
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                mapString += this.revealed[i][j];
+        for(int i = 0; i < 5; i++){                 //do not forget to remove this
+            for(int j = 0; j < 5; j++){             //do not forget to remove this
+                mapString += this.revealed[i][j];   //do not forget to remove this
             }
-            mapString += '\n';
+            mapString += '\n';                      //do not forget to remove this
         }
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                mapString += this.map[i][j];
+        for(int i = 0; i < 5; i++){                 //do not forget to remove this
+            for(int j = 0; j < 5; j++){             //do not forget to remove this
+                mapString += this.map[i][j];        //do not forget to remove this
             }
-            mapString += '\n';
+            mapString += '\n';                      //do not forget to remove this
         }
         return mapString;
     }
 
+    /**
+    * Finds 's' or start location on map
+    * @return Point location of start
+    */
     public Point findStart(){
         int x = 0;
         int y = 2;
@@ -95,10 +119,18 @@ public class Map {
         return start;
     }
 
+    /**
+    * Sets a location in revealed array to true
+    * @param p Point location to reveal
+    */
     public void reveal(Point p){
         this.revealed[p.y][p.x] = true;
     }
 
+    /**
+    * Replaces a location with 'n' for 'nothing'
+    * @param p Point location to remove
+    */
     public void removeCharAtLoc(Point p){
         this.map[p.y][p.x] = 'n';
     }
