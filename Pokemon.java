31aea6df8fb34ac0f140ecdf72949c03388a8070
Phong Cao <phongcao0714@gmail.com>
2021-11-25 11:17:42 -0800

    Modified Pokemon.java to match new req

--- Pokemon.java
+++ Pokemon.java
@@ -1,6 +1,6 @@
 import java.lang.Math;
 
-public abstract class Pokemon extends Entity{
+public abstract class Pokemon extends Entity {
     /**
      * the table of damage multiplier for the different Pokemon types
      */
@@ -43,7 +43,7 @@ public abstract class Pokemon extends Entity{
     /**
      * @return the basic menu as a String
      */
-    public String getBasicMenu(){
+    public String getAttackMenu(){
         return "1. Slam\n"
         + "2. Tackle\n"
         + "3. Punch";
@@ -52,9 +52,53 @@ public abstract class Pokemon extends Entity{
     /**
      * @return the number of choices in the basic menu (3)
      */
-    public int getNumBasicMenuItems(){
+    public int getNumAttackMenuItems(){
         return 3;
     }
+
+    public String getAttackString(int atkType, int move){
+        switch (move){
+            case 1:
+                return "SLAMMED";
+            case 2:
+                return "TACKLED";
+            case 3:
+                return "PUNCHED";
+            default:
+                return "That is not a valid move.";
+        }
+    }
+
+    public int getAttackDamage(int atkType, int move){
+        switch (move){
+            case 1:
+                final int MIN_SLAM_DAMAGE = 0;
+                final int MAX_SLAM_DAMAGE = 5;
+                return (int)(Math.random() * ((MAX_SLAM_DAMAGE-MIN_SLAM_DAMAGE)+1)) 
+                        + MIN_SLAM_DAMAGE;
+            case 2:
+                final int MIN_TACKLE_DAMAGE = 2;
+                final int MAX_TACKLE_DAMAGE = 3;
+                return (int)(Math.random() * ((MAX_TACKLE_DAMAGE-MIN_TACKLE_DAMAGE)+1)) 
+                        + MIN_TACKLE_DAMAGE;
+            case 3:
+                final int MIN_PUNCH_DAMAGE = 1;
+                final int MAX_PUNCH_DAMAGE = 4;
+                return (int)(Math.random() * ((MAX_PUNCH_DAMAGE-MIN_PUNCH_DAMAGE)+1)) 
+                        + MIN_PUNCH_DAMAGE;
+            default:
+                return -1;
+        }
+    }
+
+    public double getAttackMultipier(Pokemon p, int atkType){
+        return battleTable[this.getType()][p.getType()];
+    }
+
+    public int getAttackBonus(int atkType){
+        return 1; //TODO: Fix this later
+    }
+
 
     /**
      * attack a Pokemon with a move given as an int 
@@ -62,31 +106,26 @@ public abstract class Pokemon extends Entity{
      * 2: tackle
      * 3: punch
      * @param p the Pokemon to be attacked
+     * @param atkType the attack type
      * @param move the move to attack the Pokemon
      * @return a String message confirming the attack
      */
-    public String basicAttack(Pokemon p, int move){
-        switch (move){
-            case 1:
-                System.out.println(slam(p));
-                break;
-            case 2:
-                System.out.println(tackle(p));
-                break;
-            case 3:
-                System.out.println(punch(p));
-                break;
-            default:
-                return "That is not a valid move.";
-        }
-        return p.toString();
+    public String attack(Pokemon p, int atkType, int move){
+        int damage = (int)
+            (this.getAttackDamage(atkType, move) 
+            * this.getAttackMultipier(p, atkType) 
+            + getAttackBonus(atkType));
+
+        p.takeDamage(damage);
+        return p.getName() + "is " + getAttackString(atkType, move) + " by " 
+            + this.getName() + " and takes " + damage + "damage";
 
     }
 
     /**
      * @return the basic attack menu as a String
      */
-    public String getAttackMenu(){
+    public String getAttackTypeMenu(){
         return "1. Basic Attack\n"
         + "2. Special Attack";
     }
@@ -94,7 +133,7 @@ public abstract class Pokemon extends Entity{
     /**
      * @return the number of choices in the basic attack menu (2)
      */
-    public int getNumAttackMenuItems(){
+    public int getNumAttackTypeMenuItems(){
         return 2;
     }
 
@@ -104,15 +143,15 @@ public abstract class Pokemon extends Entity{
      * @param p the Pokemon to be slammed
      * @return a String message confirming the slam
      */
-    public String slam(Pokemon p){
-        final int MIN_DAMAGE = 0;
-        final int MAX_DAMAGE = 5;
-        int damage = 
-            (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
-            + MIN_DAMAGE;
-        p.takeDamage(damage);
-        return p.getName() + " is SLAMMED by " + this.getName() + " and takes " +  damage + " damage.";
-    }
+    // public String slam(Pokemon p){
+    //     final int MIN_DAMAGE = 0;
+    //     final int MAX_DAMAGE = 5;
+    //     int damage = 
+    //         (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
+    //         + MIN_DAMAGE;
+    //     p.takeDamage(damage);
+    //     return p.getName() + " is SLAMMED by " + this.getName() + " and takes " +  damage + " damage.";
+    // }
 
     /**
      * carry out a tackle attack on the given Pokemon
@@ -120,15 +159,15 @@ public abstract class Pokemon extends Entity{
      * @param p the Pokemon to be tackled
      * @return a String message confirming the tackle
      */
-    public String tackle(Pokemon p){
-        final int MIN_DAMAGE = 2;
-        final int MAX_DAMAGE = 3;
-        int damage = 
-            (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
-            + MIN_DAMAGE;
-        p.takeDamage(damage);
-        return p.getName() + " is TACKLED by " + this.getName() + " and takes " +  damage + " damage.";
-    }
+    // public String tackle(Pokemon p){
+    //     final int MIN_DAMAGE = 2;
+    //     final int MAX_DAMAGE = 3;
+    //     int damage = 
+    //         (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
+    //         + MIN_DAMAGE;
+    //     p.takeDamage(damage);
+    //     return p.getName() + " is TACKLED by " + this.getName() + " and takes " +  damage + " damage.";
+    // }
 
     /**
      * carry out a punch attack on the given Pokemon
@@ -136,15 +175,16 @@ public abstract class Pokemon extends Entity{
      * @param p the Pokemon to be punched
      * @return a String message confirming the slam
      */
-    public String punch(Pokemon p){
-        final int MIN_DAMAGE = 1;
-        final int MAX_DAMAGE = 4;
-        int damage = 
-            (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
-            + MIN_DAMAGE;
-        p.takeDamage(damage);
-        return p.getName() + " is PUNCHED by " + this.getName() + " and takes " +  damage + " damage.";
-    }
+    // public String punch(Pokemon p){
+    //     final int MIN_DAMAGE = 1;
+    //     final int MAX_DAMAGE = 4;
+    //     int damage = 
+    //         (int)(Math.random() * ((MAX_DAMAGE-MIN_DAMAGE)+1)) 
+    //         + MIN_DAMAGE;
+    //     p.takeDamage(damage);
+    //     return p.getName() + " is PUNCHED by " + this.getName() + " and takes " +  damage + " damage.";
+    // }
+
 
     /**
      * convert the Pokemon's type to an int and return it
